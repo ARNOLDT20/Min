df8760923913e48fa06a1e567f3d0043752a14a3
Sila-Md <hackersila2@gmail.com>
2025-11-14 23:19:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.listen(PORT, '0.0.0.0', () => {
     console.log(`
 Don't Forget To Give Star ‼️
 
-> 𝐏𝐎𝐖𝐄𝐑𝐃 𝘽𝙔 𝐀𝐒𝐇𝐈𝐘𝐀-𝐌𝐃 🥷🇱🇰
+> 𝐏𝐎𝐖𝐄𝐑𝐃 𝘽𝙔 𝙎𝙄𝙇𝘼-𝐌𝐃 🥷🇱🇰
 
 Server running on http://0.0.0.0:` + PORT);
 });
